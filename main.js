e89e06814456719ca81a5768af3e879f51597e0c
Yevhen <benatanlev@icloud.com>
2025-06-06 13:33:02 +0300

    +

---
+++ main.js
@@ -0,0 +1,3 @@
+const logger = require("./module");
+logger.info("info function");
+logger.log("log function");
---
+++ module.js
@@ -0,0 +1,10 @@
+const info = (msg) => {
+  console.log(`Info: ${msg}`);
+};
+const log = (msg) => {
+  console.log(`Log: ${msg}`);
+};
+module.exports = {
+  info,
+  log,
+};
